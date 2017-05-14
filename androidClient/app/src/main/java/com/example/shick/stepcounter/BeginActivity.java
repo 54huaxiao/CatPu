415a5d0be776e1ab6736db516fea251ec5f0549e
Jack <im_jack@qq.com>
2017-05-14 16:54:14 +0800

    fix structures and add comments on BeginActivity

--- androidClient/app/src/main/java/com/example/shick/stepcounter/BeginActivity.java
+++ androidClient/app/src/main/java/com/example/shick/stepcounter/BeginActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
@@ -26,57 +27,40 @@ import java.util.ArrayList;
  * Created by Vincent on 2016/12/19.
  */
 
-public class BeginActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener, OnItemClickListener {
+public class BeginActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, OnItemClickListener {
     private ConvenientBanner convenientBanner;//顶部广告栏控件
     private ArrayList<Integer> localImages = new ArrayList<Integer>();
     private FloatingActionButton change;
     private int click = 0;
     private ArrayList<String> transformerList = new ArrayList<String>();
+    private static int AUTO_TURNING_FREQ = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.begin_layout);
-        initViews();
         init();
-        change = (FloatingActionButton) findViewById(R.id.change);
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                click = (++click) % transformerList.size();
-                String transforemerName = transformerList.get(click);
-                try {
-                    Class cls = Class.forName("com.ToxicBakery.viewpager.transforms." + transforemerName);
-                    ABaseTransformer transforemer= (ABaseTransformer)cls.newInstance();
-                    convenientBanner.getViewPager().setPageTransformer(true,transforemer);
+    }
 
-                    //部分3D特效需要调整滑动速度
-                    if(transforemerName.equals("StackTransformer")){
-                        convenientBanner.setScrollDuration(1200);
-                    }
-
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    private void init(){
+        initViews();
+        initImageLoader();
+        loadTestData();
+        initListeners();
     }
 
     private void initViews() {
         convenientBanner = (ConvenientBanner) findViewById(R.id.convenientBanner);
-        String transforemerName = ForegroundToBackgroundTransformer.class.getSimpleName();
+        String transformerName = ForegroundToBackgroundTransformer.class.getSimpleName();
+        change = (FloatingActionButton) findViewById(R.id.change);
         try {
-            Class cls = Class.forName("com.ToxicBakery.viewpager.transforms." + transforemerName);
-            ABaseTransformer transforemer= (ABaseTransformer)cls.newInstance();
-            convenientBanner.getViewPager().setPageTransformer(true,transforemer);
+            Class cls = Class.forName("com.ToxicBakery.viewpager.transforms." + transformerName);
+            ABaseTransformer transformer = (ABaseTransformer)cls.newInstance();
+            convenientBanner.getViewPager().setPageTransformer(true,transformer);
 
             //部分3D特效需要调整滑动速度
-            if(transforemerName.equals("StackTransformer")){
+            if(transformerName.equals("StackTransformer")){
                 convenientBanner.setScrollDuration(1200);
             }
 
@@ -89,20 +73,30 @@ public class BeginActivity extends ActionBarActivity implements ViewPager.OnPage
         }
     }
 
-    private void init(){
-        initImageLoader();
-        loadTestDatas();
-        //本地图片例子
-        convenientBanner.setPages(
-                new CBViewHolderCreator<LocalImageHolderView>() {
-                    @Override
-                    public LocalImageHolderView createHolder() {
-                        return new LocalImageHolderView();
+    private void initListeners() {
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                click = (++click) % transformerList.size();
+                String transformerName = transformerList.get(click);
+                try {
+                    Class cls = Class.forName("com.ToxicBakery.viewpager.transforms." + transformerName);
+                    ABaseTransformer transformer = (ABaseTransformer)cls.newInstance();
+                    convenientBanner.getViewPager().setPageTransformer(true,transformer);
+
+                    //部分3D特效需要调整滑动速度
+                    if(transformerName.equals("StackTransformer")){
+                        convenientBanner.setScrollDuration(1200);
                     }
-                }, localImages)
-                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-                .setPageIndicator(new int[]{R.mipmap.ic_page_indicator, R.mipmap.ic_page_indicator_focused})
-                .setOnItemClickListener(this);
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     //初始化网络图片缓存库
@@ -121,11 +115,22 @@ public class BeginActivity extends ActionBarActivity implements ViewPager.OnPage
         ImageLoader.getInstance().init(config);
     }
 
-    private void loadTestDatas() {
-        //本地图片集合
+    private void loadTestData() {
+        // load local images
         for (int position = 0; position < 6; position++)
             localImages.add(getResId("ic_test_" + position, R.mipmap.class));
 
+        //本地图片例子
+        convenientBanner.setPages(
+                new CBViewHolderCreator<LocalImageHolderView>() {
+                    @Override
+                    public LocalImageHolderView createHolder() {
+                        return new LocalImageHolderView();
+                    }
+                }, localImages)
+                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+                .setPageIndicator(new int[]{R.mipmap.ic_page_indicator, R.mipmap.ic_page_indicator_focused})
+                .setOnItemClickListener(this);
 
         //各种翻页效果
         transformerList.add(DefaultTransformer.class.getSimpleName());
@@ -161,37 +166,38 @@ public class BeginActivity extends ActionBarActivity implements ViewPager.OnPage
         }
     }
 
-    // 开始自动翻页
+    // start auto turning pages every 5 seconds
     @Override
     protected void onResume() {
         super.onResume();
-        convenientBanner.startTurning(5000);
+        convenientBanner.startTurning(AUTO_TURNING_FREQ);
     }
 
-    // 停止自动翻页
+    // stop auto turning pages
     @Override
     protected void onPause() {
         super.onPause();
         convenientBanner.stopTurning();
     }
 
-
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+//      pass
     }
 
     @Override
     public void onPageSelected(int position) {
-        Toast.makeText(this,"监听到翻到第"+position+"了",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"jump to page " + position + ".",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
+//        pass
     }
 
     @Override
     public void onItemClick(int position) {
+//        jump to MainActivity
         Intent intent = new Intent(BeginActivity.this, MainActivity.class);
         startActivity(intent);
     }
