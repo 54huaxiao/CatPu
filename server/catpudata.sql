9cc5a42d19d97aa827146f5cd84ccab5b9f95b24
Hoery <hoeryy@gmail.com>
2017-06-19 20:59:12 +0800

    update catpudata.sql

--- server/catpudata.sql
+++ server/catpudata.sql
@@ -35,3 +35,23 @@ CREATE TABLE IF NOT EXISTS `userlist` (
 --
 -- 转存表中的数据 `userlist`
 --
+
+CREATE TABLE IF NOT EXISTS `RunTable` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`date` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`time` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`distance` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`username` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`_order` int NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `username` (`username`),
+	CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `userlist` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `MapTable` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`_order` int NOT NULL,
+	`latitude` double NOT NULL,
+	`longitude` double NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
